100cab38b711c8fae72de8a0d1a0f0f49b557ba2
shurst-ma <shurst@mapaction.org>
2020-12-05 21:49:26 +0000

    Fix flake8

--- mapactionpy_arcpro/arcpro_runner.py
+++ mapactionpy_arcpro/arcpro_runner.py
@@ -339,18 +339,22 @@ class ArcProRunner(BaseRunnerPlugin):
 
     def exportJpeg(self, coreFileName, exportDirectory, aprx, exportParams):
         # JPEG
-        jpgFileName = coreFileName + "-" + exportParams.get("jpgresolutiondpi", str(self.hum_event.default_jpeg_res_dpi)) + "dpi.jpg"
+        jpgFileName = coreFileName + "-" + \
+            exportParams.get("jpgresolutiondpi", str(self.hum_event.default_jpeg_res_dpi)) + "dpi.jpg"
         jpgFileLocation = os.path.join(exportDirectory, jpgFileName)
         exportParams["jpgFileName"] = jpgFileName
         Layout = aprx.listLayouts()[0]
-        Layout.exportToJPEG(jpgFileLocation, resolution=int(exportParams.get("jpgresolutiondpi", str(self.hum_event.default_jpeg_res_dpi))))
+        Layout.exportToJPEG(jpgFileLocation, resolution=int(exportParams.get(
+            "jpgresolutiondpi", str(self.hum_event.default_jpeg_res_dpi))))
         jpgFileSize = os.path.getsize(jpgFileLocation)
         exportParams["jpgFileSize"] = jpgFileSize
         return jpgFileLocation
 
     def exportPdf(self, coreFileName, exportDirectory, aprx, exportParams):
         # PDF
-        pdfFileName = coreFileName + "-" + exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi)) + "dpi.pdf"
+        pdfFileName = coreFileName + "-" + \
+            exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi)) + "dpi.pdf"
+
         pdfFileLocation = os.path.join(exportDirectory, pdfFileName)
         exportParams["pdfFileName"] = pdfFileName
 
@@ -358,7 +362,7 @@ class ArcProRunner(BaseRunnerPlugin):
         # https://pro.arcgis.com/en/pro-app/arcpy/mapping/mapseries-class.htm
 
         # exports only the selected pages to a single, multipage PDF file:
-        if not Layout.mapSeries is None:
+        if Layout.mapSeries is not None:
             ms = Layout.mapSeries
             if ms.enabled and (exportParams['productType'] == "atlas"):
                 # fields = arcpy.ListFields(fc, 'Flag')
@@ -366,16 +370,20 @@ class ArcProRunner(BaseRunnerPlugin):
                     for pageNum in range(1, ms.pageCount + 1):
                         ms.currentPageNumber = pageNum
                         seriesMapName = slugify(getattr(ms.pageRow, ms.pageNameField.name))
-                        seriesPdfFileName = coreFileName + "-mapbook-" + seriesMapName + "-" + exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi)) + "dpi.pdf"
+                        seriesPdfFileName = coreFileName + "-mapbook-" + seriesMapName + "-" + \
+                            exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi)) + "dpi.pdf"
                         seriesPdfFileLocation = os.path.join(exportDirectory, seriesPdfFileName)
-                        ms.exportToPDF(seriesPdfFileLocation, "CURRENT", resolution=int(exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi))))
+                        ms.exportToPDF(seriesPdfFileLocation, "CURRENT", resolution=int(
+                            exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi))))
                 else:
-                    seriesPdfFileName = coreFileName + "-mapbook-" + exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi)) + "dpi.pdf"
+                    seriesPdfFileName = coreFileName + "-mapbook-" + \
+                        exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi)) + "dpi.pdf"
                     seriesPdfFileLocation = os.path.join(exportDirectory, seriesPdfFileName)
-                    ms.exportToPDF(seriesPdfFileLocation, "ALL", resolution=int(exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi))))
+                    ms.exportToPDF(seriesPdfFileLocation, "ALL", resolution=int(
+                        exportParams.get("pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi))))
 
-        Layout.exportToPDF(pdfFileLocation, resolution=int(exportParams.get("pdfresolutiondpi",
-            str(self.hum_event.default_pdf_res_dpi))))
+        Layout.exportToPDF(pdfFileLocation, resolution=int(exportParams.get(
+            "pdfresolutiondpi", str(self.hum_event.default_pdf_res_dpi))))
 
         pdfFileSize = os.path.getsize(pdfFileLocation)
         exportParams["pdfFileSize"] = pdfFileSize
@@ -383,12 +391,14 @@ class ArcProRunner(BaseRunnerPlugin):
 
     def exportEmf(self, coreFileName, exportDirectory, aprx, exportParams):
         # EMF
-        emfFileName = coreFileName + "-" + exportParams.get("emfresolutiondpi", str(self.hum_event.default_emf_res_dpi)) + "dpi.emf"
+        emfFileName = coreFileName + "-" + \
+            exportParams.get("emfresolutiondpi", str(self.hum_event.default_emf_res_dpi)) + "dpi.emf"
         emfFileLocation = os.path.join(exportDirectory, emfFileName)
         exportParams["emfFileName"] = emfFileName
 
         Layout = aprx.listLayouts()[0]
-        Layout.exportToEMF(emfFileLocation, resolution=int(exportParams.get("emfresolutiondpi", str(self.hum_event.default_emf_res_dpi))))
+        Layout.exportToEMF(emfFileLocation, resolution=int(exportParams.get(
+            "emfresolutiondpi", str(self.hum_event.default_emf_res_dpi))))
 
         emfFileSize = os.path.getsize(emfFileLocation)
         exportParams["emfFileSize"] = emfFileSize
